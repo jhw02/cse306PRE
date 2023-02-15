196d57734b0d66cd6fd20890d917b7b45d19db72
Jia Hao Hao <78567216+jhw02@users.noreply.github.com>
2023-02-15 22:04:07 +0000

    apply max and min API to CLI, and bugfix for not compare element in first row

--- .ccls-cache/@home@runner@306exp/main.c
+++ .ccls-cache/@home@runner@306exp/main.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
   char f[] = "-f\0";
   char r[] = "-r\0";
   char h[] = "-h\0";
-
+  char ma[] = "-max\0";
+  char mi[] = "-min\0";
   FILE *fp = fopen(argv[argc - 1], "r");
 
   // Error in file opening
@@ -48,10 +49,49 @@ int main(int argc, char *argv[]) {
       printf("%d\n", count);
     } //-h
     else if (!strcmp(argv[i], h)) {
+
+    } // max
+    else if (!strcmp(argv[i], ma)) {
       char row[MAXCHAR];
-      printf("%s\n", row);
-      // fgets((char *)MAXCHAR, MAXCHAR, fp);
-      printf("%s\n", row);
+      int input = atoi(argv[i + 1]); // user input field
+      char *firstrow = fgets(row, MAXCHAR, fp);
+      char *token2 = strtok(firstrow, ",");
+
+      int max = 0;
+      for (int z = 0; z < 810; z++) {
+        // char z = getc(fp); z != EOF; z = getc(fp)
+        firstrow = fgets(firstrow, MAXCHAR, fp);
+        token2 = strtok(firstrow, ",");
+        printf("%s\n", token2);
+        for (int j = 1; j < input; j++) {
+          token2 = strtok(NULL, ",");
+        }
+        if (atoi(token2) > max) {
+          max = atoi(token2);
+        }
+      }
+      i++;
+      printf("The Max value of %d is %d\n", input, max);
+    } // min
+    else if (!strcmp(argv[i], mi)) {
+      char row[MAXCHAR];
+      int input = atoi(argv[i + 1]); // user input field
+      char *firstrow = fgets(row, MAXCHAR, fp);
+      char *token2 = strtok(firstrow, ",");
+      int min = 999999999;
+      for (int z = 0; z < 810; z++) {
+
+        firstrow = fgets(firstrow, MAXCHAR, fp);
+        token2 = strtok(firstrow, ",");
+        for (int j = 1; j < input; j++) {
+          token2 = strtok(NULL, ",");
+        }
+        if (atoi(token2) < min) {
+          min = atoi(token2);
+        }
+      }
+      i++;
+      printf("The Min value of %d is %d\n", input, min);
     }
   }
   fclose(fp);
